9aed4e8041bc046a258a115a41964bb8c8da3f5d
RealRiga <116930426+RealRiga@users.noreply.github.com>
2025-01-30 14:40:50 +0100

    africa lagos moment time update

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const moment = require("moment-timezone");
 
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-    // This dynamically generate current date and time
-  const currentDatetime = new Date().toISOString();
+   // This dynamically generate current date and time of Africa/Lagos
+  const currentDateTime = moment().tz("Africa/Lagos").format();
 
   res.json({
     email: "adeolayisa32@gmail.com",
